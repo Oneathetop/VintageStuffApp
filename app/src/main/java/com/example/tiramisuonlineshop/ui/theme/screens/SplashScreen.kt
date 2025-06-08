5fb196a9950004257aaafad3ae0a7ec2d3d908cc
Oneathetop <dinindukumarathilake123@gmail.com>
2025-06-08 08:57:25 +0530

    Removing hard coded texts and implementing fetching the texts from the strings.xml

--- app/src/main/java/com/example/tiramisuonlineshop/ui/theme/screens/SplashScreen.kt
+++ app/src/main/java/com/example/tiramisuonlineshop/ui/theme/screens/SplashScreen.kt
@@ -3,7 +3,6 @@ package com.example.tiramisuonlineshop.ui.theme.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
