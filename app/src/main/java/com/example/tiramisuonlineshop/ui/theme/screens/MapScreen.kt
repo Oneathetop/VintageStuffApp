c93a01928bb2e8393e843b13f634babd9e56735c
Oneathetop <dinindukumarathilake123@gmail.com>
2025-07-09 13:41:07 +0530

    Adding the map

--- app/src/main/java/com/example/tiramisuonlineshop/ui/theme/screens/MapScreen.kt
+++ app/src/main/java/com/example/tiramisuonlineshop/ui/theme/screens/MapScreen.kt
@@ -1,2 +1,41 @@
 package com.example.tiramisuonlineshop.ui.theme.screens
 
+import android.annotation.SuppressLint
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.navigation.NavHostController
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MapScreen(navController: NavHostController) {
+    Scaffold(
+        topBar = { TopAppBar(title = { Text("Store Locations") },
+            navigationIcon = {
+                IconButton(onClick = { navController.popBackStack() }) {
+                    Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+                }
+            }) }
+    ) {
+        AndroidView(factory = { ctx ->
+            WebView(ctx).apply {
+                webViewClient = WebViewClient()
+                settings.javaScriptEnabled = true
+                settings.setSupportZoom(true)
+                settings.builtInZoomControls = true
+                settings.displayZoomControls = false
+                loadUrl("file:///assets/www/map.html")
+            }
+        })
+    }
+}
